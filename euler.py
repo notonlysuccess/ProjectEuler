aee76d9cd5afb5d876f61a8359c4e403225a8b83
notonlysuccess <notonlysuccess@gmail.com>
2013-02-20 22:06:55 +0800

    update

--- euler.py
+++ euler.py
@@ -1,3 +1,10 @@
+
+[ProjectEuler](http://projecteuler.net) solve use python
+
+![Logo](http://projecteuler.net/images/pe_banner_light.png)  
+![euler](http://projecteuler.net/images/euler_portrait.png)
+
+    print sum(x for x in range(1000) if x % 3 == 0 or x % 5 == 0)
 from math import *
 from operator import *
 
